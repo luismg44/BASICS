6489f54869ad2193e2148f30a0161dcf7fb94f59
luismg44 <luis.moralesgrijalva@gmail.com>
2023-04-30 00:56:41 -0600

    ADD: operators

---
+++ Basic-python/Operators.py
@@ -0,0 +1,21 @@
+addition = 10 + 5 # Esto dara como resultado 15
+substraction = 10 - 5 # Esto dara como resultado 5
+multiplication = 10 * 5 # Esto dara como resultado 50
+
+### Comparison operators
+age = 10 == 5 # Esto dara False
+age = 10 != 5 # Esto dara True 
+age = 10 > 5 # Esto dara True
+
+### Logical operators
+age_boy = 10
+age_girl = 14
+
+age_boy > 8 and age_girl > 8 # Dara resultado True
+
+### Assignment operators
+age = 10 # Dara resultado 10
+
+age += 5 # Dara resultado 15
+age -+ 5 # Dara resultado 10
+age *= 5 # Dara resultado 50
