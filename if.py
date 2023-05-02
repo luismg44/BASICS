78144ae44d72aaeac85286711aea42df7bee9a1f
luismg44 <luis.moralesgrijalva@gmail.com>
2023-05-01 18:01:48 -0600

    ADD: homework if, elif, else

---
+++ if.py
@@ -0,0 +1,20 @@
+# Ejemplo 1
+age = 61
+
+if age > 18:
+    print("Puede ver la pelicula")
+
+# Ejemplo 2
+if age > 60:
+    print("Puede ver la pelicula con descuento")
+elif age > 18:
+    print("Puedes ver la pelicula")
+
+# Ejemplo 3
+if age > 60:
+    print("Puede ver la pelicula con descuento")
+elif age > 18:
+    print("Puedes ver la pelicula")
+else:
+    print("No puedes entrar")
+    print("Ve a otro lado")
